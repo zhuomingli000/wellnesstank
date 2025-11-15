1319d959ba49563a2275704180a21a7da0578b2a
Zhuoming Li <zhuoming@meta.com>
2025-11-15 09:52:27 -0800

    Initial Commit

---
+++ WellnessTank/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WellnessTank
+//
+//  Created by Zhuoming Li on 11/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WellnessTank/WellnessTankApp.swift
@@ -0,0 +1,17 @@
+//
+//  WellnessTankApp.swift
+//  WellnessTank
+//
+//  Created by Zhuoming Li on 11/15/25.
+//
+
+import SwiftUI
+
+@main
+struct WellnessTankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
